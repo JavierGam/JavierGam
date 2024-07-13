7b204a609800a4e1c8dd6ab70d6c0573aac4e2ae
JavierGam <89045168+JavierGam@users.noreply.github.com>
2024-07-12 17:16:36 -0700

    Create Nameapp.py

---
+++ Nameapp.py
@@ -0,0 +1,6 @@
+print("***Welcome to the name app***")
+first_name = input("what is your first name??")
+last_name = input("what is your last name??")
+
+full_name = first_name + " " + last_name
+print("Hi there, " + full_name)
